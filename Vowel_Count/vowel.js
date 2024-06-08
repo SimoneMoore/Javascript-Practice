79277cb37da79f866b6d88663dec560581ea2027
Simone Moore <moores0929@gmail.com>
2024-06-08 17:34:43 -0400

    06/08_p5

---
+++ Vowel_Count/vowel.js
@@ -0,0 +1,15 @@
+function getCount(str) {
+    let vowelCount = 0;
+    for ( let i = 0; i === "a" || i === "e" || i === "i" || i === "o" || i === "u" ; i++ ) {
+        if (true) {
+            vowelCount= vowelCount +  welCount)
+        } else {
+            console.log("No vowels here!");
+        }
+    }
+    console.log(vowelCount)
+    return vowelCount;
+}
+return getCount("Baba Ganoush")
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels (but not y).
