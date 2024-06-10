f9d54eccc2e798b68a6f5cacc41004d22dc649de
Simone Moore <moores0929@gmail.com>
2024-06-10 00:14:09 -0400

    completed

--- LikeCount/likenum.js
+++ LikeCount/likenum.js
@@ -1,5 +1,27 @@
+function likes(names) {
+    let message= "";
+    switch(names.length) {
+        case 0:
+            message= ("no one likes this");
+            break;
+        case 1:
+            message= names[0] + " likes this";
+            break;
+        case 2:
+            message= names[0] + " and" + names[1] + " like this";
+            break;
+        case 3:
+            message= names[0] + "," + names[1] + "and" + names[2] + " like this";
+            break;
+        default:
+            message= names[0] + ", " + names[1] + " and " + (names.length -2) + " others like this";
+             break;
+    }
+    console.log(message);
+    return message
+}
 
-
+likes(["john", "paul", "joseph", "mary"])
 // Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 // []                                -->  "no one likes this"
 // ["Peter"]                         -->  "Peter likes this"
